e14ceff03eaf69bc8871d1ef835e503d79706ddd
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-30 12:11:13 +0545

    made user-choice-cli based input for width and format of the output images

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const _ = require('lodash')
 const imageUrls = require('./imageUrls.json');
 const DOWNLOAD_FOLDER = 'downloads';
+const sharp = require('sharp');
+const prompts = require('prompts');
+
+let width;
+let format = "";
 
 //check for downloads folder and create downloadeds folder incase, it doesn't exist
 if (!fs.existsSync(DOWNLOAD_FOLDER)) {
@@ -10,7 +15,7 @@ if (!fs.existsSync(DOWNLOAD_FOLDER)) {
 
 //returning path to downloads folder with index of element as a name of file and png as its type
 function getIndexedFileName(i) {
-    return ("./downloads/" + i + ".png");
+    return ("./downloads/" + i + "." + format);
 }
 
 //asynchronous function to download images from imageUrls.json file
@@ -22,14 +27,45 @@ async function downloadImages(imageUrls) {
         const blob = await response.blob();
         const arrayBuffer = await blob.arrayBuffer();
         const buffer = Buffer.from(arrayBuffer);
-        await fs.writeFileSync(fileNameWithPath, buffer);
+        await sharp(buffer).resize(width).toFile(fileNameWithPath, function (err) {
+            if (err) { console.log(err) };
+        });
+        // await fs.writeFileSync(fileNameWithPath, buffer);
         //by such way of changing response in buffer we can easily write it in output image file
     }
 }
 
-function start() {
+async function start() {
     const uniqImageUrls = _.uniq(imageUrls);//removes duplicates
+
+    //asking user for width and saving it in width variable
+    const userInputWidth = await prompts({
+        name: 'value',
+        type: 'number',
+        message: 'enter width of image you want to download:\n',
+    });
+    width = userInputWidth.value;
+
+    //making user choose format from list then starting download process
+    const userInputFormat = await prompts({
+        name: 'value',
+        type: 'select',
+        message: 'choose image format from the list below:\n',
+        choices: [
+            { title: 'JPEG', value: 'jpeg' },
+            { title: 'PNG', value: 'png' },
+            { title: 'WEBP', value: 'webp' },
+            { title: 'AVIF', value: 'avif' },
+            { title: 'TIFF', value: 'tiff' },
+            { title: 'GIF', value: 'gif' },
+            { title: 'DZI', value: 'dzi' }
+        ]
+    });
+    format = userInputFormat.value;
+
     downloadImages(uniqImageUrls);
+
+    console.log('Download complete!');
 }
 
 start();
